c1a4b832c3d6ddbcacd38ec98f212f758433a241
code0wl <o.lodriguez@gmail.com>
2018-01-07 20:49:50 +0100

    mongo

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://localhost:27017/mydb";
+
+MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    console.log("Database created!");
+    db.close();
+});
